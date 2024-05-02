9abf855c668e9321811b565f8b8a2323f4f10b5f
Mounesh kp <114060940+mouneshkp@users.noreply.github.com>
2024-05-02 15:45:08 +0530

    fully updated ui tested its working fine

--- src/App.js
+++ src/App.js
@@ -1,67 +1,85 @@
 import React, { useState } from 'react';
-import { Button, Modal, Progress } from 'antd';
+import { Button, Modal, Progress, Upload } from 'antd';
+import * as XLSX from 'xlsx';
 import './App.css';
 import logoImage from './make-my-trip-1-260x146-removebg-preview.png';
-
+import fetch from 'cross-fetch';
 
 function App() {
+  // State variables
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
-  const [expenseClientId, setExpenseClientId] = useState('');
-  const [externalOrgId, setExternalOrgId] = useState('');
   const [collectionName, setCollectionName] = useState('');
-  const [Row_Labels, setRow_Labels] = useState('');
-  const [Org_name, setOrg_name] = useState('');
-  const [Admin_Email_1, setAdmin_Email_1] = useState('');
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [progressPercent, setProgressPercent] = useState(0);
+  const [fileData, setFileData] = useState(null);
   const [openModal, setOpenModal] = useState(false);
   const [modalMessage, setModalMessage] = useState('');
   const [errorModalVisible, setErrorModalVisible] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
 
+  // Function to handle file upload
+  const handleFileUpload = (file) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const workbook = XLSX.read(e.target.result, { type: 'binary' });
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+      const data = XLSX.utils.sheet_to_json(sheet);
+      const transformedData = data.map(row => ({
+        Row_Labels: parseInt(row.Row_Labels),
+        Org_name: row.Org_name && row.Org_name.trim(),
+        Admin_Email_1: row.Admin_Email_1 && row.Admin_Email_1.trim(),
+        expenseClientId: row.Expense_Client_Id && row.Expense_Client_Id.trim(),
+        externalOrgId: row.External_Org_Id && row.External_Org_Id.trim(),
+      }));
+      setFileData(transformedData);
+    };
+    reader.readAsBinaryString(file);
+  };
+
+  // Function to handle form submission
   const handleSubmit = async (event) => {
     event.preventDefault();
     setIsSubmitted(true);
-  
     const startDateObj = new Date(startDate);
     const endDateObj = new Date(endDate);
-  
-    // Calculate the difference in months between start and end dates
     const monthDiff = (endDateObj.getFullYear() - startDateObj.getFullYear()) * 12 +
-      endDateObj.getMonth() - startDateObj.getMonth();
-  
-    // Check if the difference is exactly 12 months
+    endDateObj.getMonth() - startDateObj.getMonth();
+
     if (monthDiff !== 12) {
       setErrorMessage('Start and end dates must be exactly 12 months apart.');
       setErrorModalVisible(true);
       setIsSubmitted(false);
       return;
     }
-  
-    // Loop 12 times to submit data for each month
+    for (let i = 0; i < fileData.length; i++) {
+      const rowData = fileData[i];
+    // Loop for each month and submit data
     for (let i = 0; i < 12; i++) {
       const startOfMonth = new Date(startDateObj);
       startOfMonth.setMonth(startDateObj.getMonth() + i);
-  
+
       const endOfMonth = new Date(startOfMonth);
       endOfMonth.setMonth(startOfMonth.getMonth() + 1);
-      endOfMonth.setDate(endOfMonth.getDate() - 1); // Set to last day of the month
-  
-      const data = {
-        start_date: startOfMonth.toISOString().slice(0, 10), // Format as YYYY-MM-DD
-        end_date: endOfMonth.toISOString().slice(0, 10), // Format as YYYY-MM-DD
-        'expense-client-id': expenseClientId,
-        'external-org-id': externalOrgId,
-        collection_name: collectionName,
-        'Admin_Email_1':Admin_Email_1,
-        'Org_name':Org_name,
-        'Row_Labels':Row_Labels
+      endOfMonth.setDate(endOfMonth.getDate() - 1);
 
+      // const rowData = fileData[i % fileData.length]; // Accessing fileData based on loop index
+      
+      // console.log('Data to senddddddddddddd:', rowData);
+      const data = {
+        start_date: startOfMonth.toISOString().slice(0, 10),
+        end_date: endOfMonth.toISOString().slice(0, 10),
+        'expense-client-id': rowData.expenseClientId,
+        'external-org-id': rowData.externalOrgId,
+        collection_name: collectionName,
+        'Admin_Email_1': rowData.Admin_Email_1,
+        'Org_name': rowData.Org_name,
+        'Row_Labels': rowData.Row_Labels
       };
-  
+
       console.log('Data to send:', data); // Log data before sending
-  
+
       try {
         const response = await fetch('http://127.0.0.1:5000/submit', {
           method: 'POST',
@@ -70,18 +88,15 @@ function App() {
           },
           body: JSON.stringify(data),
         });
-  
+
         console.log('Response:', response); // Log the response
-  
+
         const responseData = await response.json();
-  
-        // Handle successful response
+
         if (response.ok) {
-          // Log the response data for debugging
           console.log('Response Data:', responseData);
           // Handle success as needed
         } else {
-          // Handle other response statuses (e.g., error responses)
           throw new Error(responseData.message || 'Failed to submit data');
         }
       } catch (error) {
@@ -89,74 +104,66 @@ function App() {
         // Handle error here, such as showing an error message to the user
       }
     }
-  
-    // Reset the submission state
+  }
     setIsSubmitted(false);
   };
-  
 
+  // Function to handle modal cancel
+  const handleModalCancel = () => {
+    setOpenModal(false);
+    // Additional logic if needed
+    setIsSubmitted(false);
+  };
+
+  // Function to handle modal OK
   const handleModalOk = () => {
     setOpenModal(false);
     setIsSubmitted(false);
     setProgressPercent(0);
     setStartDate('');
     setEndDate('');
-    setExpenseClientId('');
-    setExternalOrgId('');
     setCollectionName('');
-    setOrg_name('');
-    setRow_Labels('');
-    setAdmin_Email_1('');
+    setFileData(null);
   };
 
-  const handleModalCancel = () => {
-    setOpenModal(false);
-    setIsSubmitted(false);
-  };
+  // JSX code for UI
 
   return (
     <div className="App">
-      <img src={logoImage} alt="Logo" style={{ position: 'absolute', left: '10px', top: '10px' }} /> {/* Add your image here */}
+      <img src={logoImage} alt="Logo" style={{ position: 'absolute', left: '10px', top: '10px' }} />
       <form className="custom-form" onSubmit={handleSubmit}>
+        <Upload
+          accept=".xlsx,.xls"
+          beforeUpload={file => {
+            handleFileUpload(file);
+            return false;
+          }}
+          showUploadList={false}
+        >
+          <Button>Upload XLSX File</Button>
+        </Upload>
+        {fileData && (
+          <div>
+            <p>File Uploaded Successfully!</p>
+            <ul>
+            </ul>
+          </div>
+        )}
         <div className="input-group">
-          <label style={{ marginLeft: "-54px" }}>Start Date :</label>
+          <label>Start Date :</label>
           <input
             type="date"
             value={startDate}
             onChange={(e) => setStartDate(e.target.value)}
-            disabled={isSubmitted}
             required
-            style={{ width: "100px" }}
           />
         </div>
         <div className="input-group">
-          <label style={{ marginLeft: "-54px" }}>End Date :</label>
+          <label>End Date :</label>
           <input
             type="date"
             value={endDate}
             onChange={(e) => setEndDate(e.target.value)}
-            disabled={isSubmitted}
-            required
-            style={{ width: "100px" }}
-          />
-        </div>
-        <div className="input-group">
-          <label>expenseClientId</label>
-          <input
-            type="text"
-            value={expenseClientId}
-            onChange={(e) => setExpenseClientId(e.target.value)}
-            disabled={isSubmitted}
-            required
-          />
-        </div>
-        <div className="input-group">
-          <label>externalOrgId :</label>
-          <input
-            type="text"
-            value={externalOrgId}
-            onChange={(e) => setExternalOrgId(e.target.value)}
-            disabled={isSubmitted}
             required
           />
         </div>
@@ -166,37 +173,6 @@ function App() {
             type="text"
             value={collectionName}
             onChange={(e) => setCollectionName(e.target.value)}
-            disabled={isSubmitted}
-            required
-          />
-        </div>
-        <div className="input-group">
-          <label>Row_Labels:</label>
-          <input
-            type="text"
-            value={Row_Labels}
-            onChange={(e) => setRow_Labels(e.target.value)}
-            disabled={isSubmitted}
-            required
-          />
-        </div>
-        <div className="input-group">
-          <label>Org_name:</label>
-          <input
-            type="text"
-            value={Org_name}
-            onChange={(e) => setOrg_name(e.target.value)}
-            disabled={isSubmitted}
-            required
-          />
-        </div>
-        <div className="input-group">
-          <label>Admin_Email_1:</label>
-          <input
-            type="text"
-            value={Admin_Email_1}
-            onChange={(e) => setAdmin_Email_1(e.target.value)}
-            disabled={isSubmitted}
             required
           />
         </div>
